bf1c5b4fe8c8b694649004bb4d25ee062801ba4d
Thanziya C N <thanziya8123@gmail.com>
2026-02-23 18:30:06 +0530

    Add files via upload

---
+++ day3_average_improved.py
@@ -0,0 +1,11 @@
+print("===== Average Calculator =====")
+a=float(input("Enter the first number:"))
+b=float(input("Enter the second number:"))
+c=float(input("Enter the third number:"))
+
+total=a+b+c
+average=total/3
+
+print(f"Total is:{total}")
+print(f"Average is:{average:.2f}")
+
